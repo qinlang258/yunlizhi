93a60a240122f8ddbeb24f96e99bdf0ced672714
root <root@jcrose>
2023-04-30 15:27:22 +0800

    add yunlizhi controller

---
+++ controllers/app_controller.go
@@ -0,0 +1,370 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/pointer"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	apinetv1 "k8s.io/api/networking/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	infrav1 "yunlizhi/api/v1"
+)
+
+// AppReconciler reconciles a App object
+type AppReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+	Log    logr.Logger
+}
+
+//+kubebuilder:rbac:groups=infra.yunlizhi.cn,resources=apps,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=infra.yunlizhi.cn,resources=apps/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=infra.yunlizhi.cn,resources=apps/finalizers,verbs=update
+//+kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=networking,resources=ingresses,verbs=get;list;watch;create;update;patch;delete
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the App object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.11.0/pkg/reconcile
+
+func (r *AppReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	ctx = context.Background()
+	log := r.Log.WithValues("yunlizhi infra app", req.NamespacedName)
+	log.Info("1. start reconcile logic")
+
+	// TODO(user): your logic here
+	instance := &infrav1.App{}
+
+	// 通过客户端工具查询，查询条件是
+	err := r.Get(ctx, req.NamespacedName, instance)
+
+	if err != nil {
+
+		// 如果没有实例，就返回空结果，这样外部就不再立即调用Reconcile方法了
+		if errors.IsNotFound(err) {
+			log.Info("2.1. instance not found, maybe removed")
+			return reconcile.Result{}, nil
+		}
+
+		log.Error(err, "2.2 error")
+		// 返回错误信息给外部
+		return ctrl.Result{}, err
+	}
+
+	log.Info("3. instance : " + instance.String())
+
+	// 查找deployment
+	deployment := &appsv1.Deployment{}
+
+	// 用客户端工具查询
+	err = r.Get(ctx, req.NamespacedName, deployment)
+
+	// 查找时发生异常，以及查出来没有结果的处理逻辑
+	if err != nil {
+		// 如果没有实例就要创建了
+		if errors.IsNotFound(err) {
+			log.Info("4. deployment not exists")
+
+			// 如果对QPS没有需求，此时又没有deployment，就啥事都不做了
+
+			// 先要创建service
+			if err = createServiceIfNotExists(ctx, r, instance, req); err != nil {
+				log.Error(err, "5.2 error")
+				// 返回错误信息给外部
+				return ctrl.Result{}, err
+			}
+
+			// 立即创建deployment
+			if err = createDeploymentIfNotExists(ctx, r, instance, req); err != nil {
+				log.Error(err, "5.3 error")
+				// 返回错误信息给外部
+				return ctrl.Result{}, err
+			}
+
+			if err = createIngressIfNotExists(ctx, r, instance, req); err != nil {
+				log.Error(err, "5.3 error")
+				// 返回错误信息给外部
+				return ctrl.Result{}, err
+			}
+
+			// 如果创建成功就更新状态
+			if err = updateStatus(ctx, r, instance); err != nil {
+				log.Error(err, "5.4. error")
+				// 返回错误信息给外部
+				return ctrl.Result{}, err
+			}
+
+			// 创建成功就可以返回了
+			return ctrl.Result{}, nil
+		} else {
+			log.Error(err, "7. error")
+			// 返回错误信息给外部
+			return ctrl.Result{}, err
+		}
+	}
+
+	// 如果查到了deployment，并且没有返回错误，就走下面的逻辑
+
+	log.Info("11. update deployment's Replicas")
+	// 通过客户端更新deployment
+	if err = r.Update(ctx, deployment); err != nil {
+		log.Error(err, "12. update deployment replicas error")
+		// 返回错误信息给外部
+		return ctrl.Result{}, err
+	}
+
+	log.Info("13. update status")
+
+	// 如果更新deployment的Replicas成功，就更新状态
+	if err = updateStatus(ctx, r, instance); err != nil {
+		log.Error(err, "14. update status error")
+		// 返回错误信息给外部
+		return ctrl.Result{}, err
+	}
+
+	return ctrl.Result{}, nil
+
+}
+
+func updateStatus(ctx context.Context, r *AppReconciler, App *infrav1.App) error {
+	log := r.Log.WithValues("func", "updateStatus")
+
+	if err := r.Status().Update(ctx, App); err != nil {
+		log.Error(err, "update instance error")
+		return err
+	}
+
+	return nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *AppReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&infrav1.App{}).
+		Complete(r)
+}
+
+func createDeploymentIfNotExists(ctx context.Context, r *AppReconciler, app *infrav1.App, req ctrl.Request) error {
+	log := r.Log.WithValues("func", "createDeployment")
+
+	deployment := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: app.Namespace,
+			Name:      app.Name,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: pointer.Int32Ptr(1),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": app.Spec.Project,
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": app.Spec.Project,
+					},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:            app.Spec.Project,
+							Image:           app.Spec.Image,
+							ImagePullPolicy: "IfNotPresent",
+							Ports: []corev1.ContainerPort{
+								{
+									Name:          "http",
+									Protocol:      corev1.ProtocolTCP,
+									ContainerPort: *app.Spec.Port,
+								},
+							},
+							Resources: corev1.ResourceRequirements{
+								Requests: corev1.ResourceList{
+									"cpu":    resource.MustParse("100m"),
+									"memory": resource.MustParse("256Mi"),
+								},
+								Limits: corev1.ResourceList{
+									"cpu":    resource.MustParse("200m"),
+									"memory": resource.MustParse("512Mi"),
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	log.Info("set reference")
+	if err := controllerutil.SetControllerReference(app, deployment, r.Scheme); err != nil {
+		log.Error(err, "SetControllerReference error")
+		return err
+	}
+	log.Info("start create deployment")
+	if err := r.Create(ctx, deployment); err != nil {
+		log.Error(err, "create deployment error")
+		return err
+	}
+	log.Info("create deployment success")
+	return nil
+}
+
+func createServiceIfNotExists(ctx context.Context, r *AppReconciler, app *infrav1.App, req ctrl.Request) error {
+	log := r.Log.WithValues("func", "createService")
+
+	service := &corev1.Service{}
+
+	err := r.Get(ctx, req.NamespacedName, service)
+
+	// 如果查询结果没有错误，证明service正常，就不做任何操作
+	if err == nil {
+		log.Info("service exists")
+		return nil
+	}
+
+	// 如果错误不是NotFound，就返回错误
+	if !errors.IsNotFound(err) {
+		log.Error(err, "query service error")
+		return err
+	}
+
+	// 实例化一个数据结构
+	service = &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: app.Namespace,
+			Name:      app.Name,
+		},
+		Spec: corev1.ServiceSpec{
+			Ports: []corev1.ServicePort{{
+				Name: "http",
+				Port: *app.Spec.Port,
+			},
+			},
+			Selector: map[string]string{
+				"app": app.Spec.Project,
+			},
+			Type: corev1.ServiceTypeNodePort,
+		},
+	}
+
+	// 这一步非常关键！
+	// 建立关联后，删除elasticweb资源时就会将deployment也删除掉
+	log.Info("set reference")
+	if err := controllerutil.SetControllerReference(app, service, r.Scheme); err != nil {
+		log.Error(err, "SetControllerReference error")
+		return err
+	}
+
+	// 创建service
+	log.Info("start create service")
+	if err := r.Create(ctx, service); err != nil {
+		log.Error(err, "create service error")
+		return err
+	}
+
+	log.Info("create service success")
+
+	return nil
+}
+
+func createIngressIfNotExists(ctx context.Context, r *AppReconciler, app *infrav1.App, req ctrl.Request) error {
+	log := r.Log.WithValues("func", "createIngress")
+
+	ingress := &apinetv1.Ingress{}
+
+	err := r.Get(ctx, req.NamespacedName, ingress)
+
+	// 如果查询结果没有错误，证明service正常，就不做任何操作
+	if err == nil {
+		log.Info("ingress exists")
+		return nil
+	}
+
+	// 如果错误不是NotFound，就返回错误
+	if !errors.IsNotFound(err) {
+		log.Error(err, "query service error")
+		return err
+	}
+
+	// 实例化一个数据结构
+	ingress.Name = app.Name
+	ingress.Namespace = app.Namespace
+	pathType := apinetv1.PathTypePrefix
+	icn := "nginx"
+	ingress.Spec = apinetv1.IngressSpec{
+		IngressClassName: &icn,
+		Rules: []apinetv1.IngressRule{
+			{
+				Host: app.Spec.Domain,
+				IngressRuleValue: apinetv1.IngressRuleValue{
+					HTTP: &apinetv1.HTTPIngressRuleValue{
+						Paths: []apinetv1.HTTPIngressPath{
+							{
+								Path:     "/",
+								PathType: &pathType,
+								Backend: apinetv1.IngressBackend{
+									Service: &apinetv1.IngressServiceBackend{
+										Name: app.Name,
+										Port: apinetv1.ServiceBackendPort{
+											Number: *app.Spec.Port,
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	// 这一步非常关键！
+	// 建立关联后，删除elasticweb资源时就会将deployment也删除掉
+	log.Info("set reference")
+	if err := controllerutil.SetControllerReference(app, ingress, r.Scheme); err != nil {
+		log.Error(err, "SetControllerReference error")
+		return err
+	}
+
+	// 创建service
+	log.Info("start create ingress")
+	if err := r.Create(ctx, ingress); err != nil {
+		log.Error(err, "create service error")
+		return err
+	}
+
+	log.Info("create service success")
+
+	return nil
+}
